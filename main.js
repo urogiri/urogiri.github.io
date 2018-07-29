31d0709336b40a07c9879a46df57f451a549d1f1
Nicole <31230052+urogiri@users.noreply.github.com>
2018-07-30 01:13:31 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r70/three.min.js"></script>
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r70/three.min.js"></script>
 
 function init() {
 	
