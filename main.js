261d4ca899327d3944da3b60dbf54002ebf71149
Nicole <31230052+urogiri@users.noreply.github.com>
2018-09-07 20:16:50 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"</script>
-<script src="https://cdn.rawgit.com/michalsnik/aos/1.0.1/dist/aos.css"</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"</script>
+
 
 console.clear();
 AOS.init({
