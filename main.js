2d3817d369b9f08130741debbc48bda52f1b6545
Nicole <31230052+urogiri@users.noreply.github.com>
2018-09-07 21:02:50 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,78 +1,50 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r70/three.min.js"</script>
 
+function init() {
+	
+	// heeere we go !
+	
+	var blue = new THREE.Color(0x7658ef);
+	var pink = new THREE.Color(0xfca4c5);
 
-console.clear();
-AOS.init({
-	offset: -50,
-  duration: 650,
-  easing: [0.4, 0.0, 0.2, 1],
-	delay: 350,
-	disable: window.innerWidth < 786
-});
-var zi9yoyw179vy9j;
-(function(d, t) {
-	var s = d.createElement(t), options = {
-		'userName':'ltrademark',
-		'formHash':'zi9yoyw179vy9j',
-		'autoResize':true,
-		'height':'1100',
-		'async':true,
-		'host':'wufoo.com',
-		'header':'hide',
-		'ssl':true
-	};
-	s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
-	s.onload = s.onreadystatechange = function() {
-		var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
-		try { zi9yoyw179vy9j = new WufooForm();zi9yoyw179vy9j.initialize(options);zi9yoyw179vy9j.display(); } catch (e) {}
-	};
-	var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
-})(document, 'script');
+	var scene = new THREE.Scene();
+	var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-$(window).scroll(function(){
-	var height = Math.round($('.hero').height());
-	var top = Math.round($(window).scrollTop());
-	if(top != 0) {
-		$('.hero').find('.main-content').addClass('fade');
-	} else {
-		$('.hero').find('.main-content').removeClass('fade');
-		$('body').velocity("stop");
+	var renderer = new THREE.WebGLRenderer({ alpha: true });
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	document.body.appendChild( renderer.domElement );
+
+	//THREEx.WindowResize(renderer, camera);
+	var shape = [];
+	geometry = new THREE.IcosahedronGeometry(2.5,0);
+	material = new THREE.MeshNormalMaterial({ color: 0x0000ff });
+	shape[0] = new THREE.Mesh( geometry, material );
+	shape[1] = new THREE.Mesh( geometry, material );
+	shape[2] = new THREE.Mesh( geometry, material );
+	shape[0].position.set(0,5,0);
+	shape[1].position.set(0,5,0);
+	shape[2].position.set(0,5,0);
+	scene.add(shape[0],shape[1],shape[2]);
+
+	var light = new THREE.PointLight(0xfca4c5);
+	light.position.set(0,250,0);
+	scene.add(light);
+
+	camera.position.set(3,4,10); // x y z
+
+	function render() {
+		requestAnimationFrame( render );
+
+		shape[0].rotation.x += 0.035;
+		shape[0].rotation.y -= 0.005;
+		shape[1].rotation.y += 0.015;
+		shape[1].rotation.z -= 0.005;
+		shape[2].rotation.z -= 0.025;
+		shape[2].rotation.x += 0.005;
+		renderer.render(scene, camera);
 	}
-});
-function componentToHex(c) {
-    var hex = c.toString(16);
-    return hex.length == 1 ? "0" + hex : hex;
+	render();
+	
 }
 
-function rgbToHex(r, g, b) {
-    return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
-function setMetaColor() {
-	var bodyRGB = window.getComputedStyle(document.body, null).getPropertyValue('background-color'),
-			bRGBstrip = bodyRGB.replace('rgb','').replace('(', '').replace(')', '').split(','),
-			bodyHEX = rgbToHex(parseInt(bRGBstrip[0]), parseInt(bRGBstrip[1]), parseInt(bRGBstrip[2]));
-	document.head.getElementsByTagName('meta')[2].setAttribute('content', bodyHEX);
-	console.log(bodyHEX);
-}
-
-$('#messageForm').on('click', function(){
-	$('.about').css('opacity', '0');
-	$('.message-form').addClass('open');
-});
-$('.dismiss-form').on('click', function(){
-	$('.about').css('opacity', '1');
-	$('.message-form').removeClass('open');
-});
-$('#thememain').on('click', function(){
-	$('.swatch').removeClass('current');
-	$(this).addClass('current');
-	$('html, body').attr('class','');
-	setMetaColor();
-});
-$('#theme1, #theme2, #theme3').on('click', function(){
-	var theme = $(this).attr('id');
-	$('.swatch').removeClass('current');
-	$(this).addClass('current');
-	$('html, body').attr('class','').addClass(theme);
-	setMetaColor();
-});
+init();
